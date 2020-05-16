ffda5da290fd6657f471bc10baafdff8e19c9aeb
Ch. Alexandru <rajdjel@gmail.com>
2020-05-16 15:18:05 +0300

    added new project: react-unsplash

---
+++ react-unsplash/precache-manifest.a6b6a87ba96e2f6030dad2377b63c0f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2de9929d61a77784bc820a9f00d9da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be5cce962a5ae8390a5",
+    "url": "/static/css/main.fd687b2f.chunk.css"
+  },
+  {
+    "revision": "d1652d1d721eb77ad260",
+    "url": "/static/js/2.957bb31d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.957bb31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be5cce962a5ae8390a5",
+    "url": "/static/js/main.2d03996d.chunk.js"
+  },
+  {
+    "revision": "4301f539c6b9d248d2ef",
+    "url": "/static/js/runtime-main.9b6c721d.js"
+  },
+  {
+    "revision": "b25bc43a21deee985378a24a78375702",
+    "url": "/static/media/bg.b25bc43a.jpg"
+  }
+]);
